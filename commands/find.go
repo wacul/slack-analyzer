b23377b8195e8c477818e8b991de48963ad344a8
Kyoichiro Yamada <master@kyoh86.dev>
2019-05-31 11:16:43 +0900

    wip: ワードを含むレスの一覧を出す

---
+++ commands/find.go
@@ -0,0 +1,71 @@
+package commands
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+func Find(slackToken string, targetChannels []string, targetWords []string, eachReport bool, allReportChannel string, oldest, latest DateTime) error {
+	channelFilter := NewStringFilter(targetChannels)
+
+	api := slack.New(slackToken)
+	channels, err := api.GetChannels(true)
+	if err != nil {
+		return fmt.Errorf("failed to get channels: %s", err)
+	}
+
+	var allWords StringCounter
+	allReportChannelID := ""
+	for _, c := range channels {
+		if c.Name == allReportChannel {
+			allReportChannelID = c.ID
+		}
+
+		if !channelFilter.Match(c.Name) {
+			continue
+		}
+		history, err := api.GetChannelHistory(c.ID, slack.HistoryParameters{
+			Oldest: fmt.Sprintf("%d.000000", oldest.Time().Unix()),
+			Latest: fmt.Sprintf("%d.000000", latest.Time().Unix()),
+			Count:  1000,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to get channel %s history: %s", c.Name, err)
+		}
+		var eachWords StringCounter
+		log.Printf("%d messages found in %s", len(history.Messages), c.Name)
+		for _, m := range history.Messages {
+			for _, w := range targetWords {
+				cnt := strings.Count(m.Text, w)
+				eachWords.Add(w, cnt)
+				allWords.Add(w, cnt)
+			}
+		}
+		if eachReport {
+			var buf bytes.Buffer
+			fmt.Fprintf(&buf, "Words in this channel from %s to %s\n", oldest.Time().Format("01-02 15:04"), latest.Time().Format("01-02 15:04"))
+			fmt.Fprintln(&buf)
+			eachWords.Fprint(&buf)
+			if _, _, err := api.PostMessage(c.ID, slack.MsgOptionText(buf.String(), false)); err != nil {
+				return fmt.Errorf("failed to post result to %s: %s", c.Name, err)
+			}
+		}
+	}
+	if allReportChannel != "" {
+		if allReportChannelID == "" {
+			return fmt.Errorf("failed to find public channel %s", allReportChannel)
+		}
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, "Words in all public channel from %s to %s\n", oldest.Time().Format("01-02 15:04"), latest.Time().Format("01-02 15:04"))
+		fmt.Fprintln(&buf)
+		allWords.Fprint(&buf)
+		if _, _, err := api.PostMessage(allReportChannelID, slack.MsgOptionText(buf.String(), false)); err != nil {
+			return fmt.Errorf("failed to post all channel report to %s: %s", allReportChannel, err)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	cmds := map[string]func() error{}
 	for _, f := range []func(*kingpin.Application) (string, func() error){
 		reaction,
+		find,
 		word,
 	} {
 		key, run := f(app)
@@ -55,6 +56,31 @@ func reaction(app *kingpin.Application) (string, func() error) {
 
 	return cmd.FullCommand(), func() error {
 		return commands.Reaction(slackToken, targetChannels, targetEmoji, eachReport, allReportChannel, oldest, latest)
+	}
+}
+
+func find(app *kingpin.Application) (string, func() error) {
+	var (
+		slackToken       string
+		targetChannels   []string
+		targetWord       []string
+		eachReport       bool
+		allReportChannel string
+
+		oldest commands.DateTime
+		latest commands.DateTime
+	)
+	cmd := app.Command("word", "Analyze words")
+	cmd.Flag("slack-token", "Slack App OAuth App Token").Envar("SLACK_TOKEN").Required().StringVar(&slackToken)
+	cmd.Flag("channel", "Target channels (not specifing, all public channels)").StringsVar(&targetChannels)
+	cmd.Flag("word", "Target word").Required().StringsVar(&targetWord)
+	cmd.Flag("each-channel-report", "Show each channel report").Default("false").BoolVar(&eachReport)
+	cmd.Flag("all-channel-report", "A channel name to post all public channel report").StringVar(&allReportChannel)
+	cmd.Flag("oldest", "A time of the oldest message that should be analyzed").Default(time.Now().Add(-7 * 24 * time.Hour).Format(commands.DateFormat)).SetValue(&oldest)
+	cmd.Flag("latest", "A time of the latest message that should be analyzed").Default(time.Now().Format(commands.DateFormat)).SetValue(&latest)
+
+	return cmd.FullCommand(), func() error {
+		return commands.Find(slackToken, targetChannels, targetWord, eachReport, allReportChannel, oldest, latest)
 	}
 }
 
